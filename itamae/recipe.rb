5de5b41f35c70c0c33137d4874cdff5d03677fc6
rabbit1031 <rabbit1031@users.noreply.github.com>
2018-07-31 21:52:03 +0900

    added php install command to recipe

--- itamae/recipe.rb
+++ itamae/recipe.rb
@@ -1,14 +1,21 @@
 execute "yum update" do
   user "root"
-  command "yum update --assumeyes"
+  command "yum update -y --disablerepo='percona-*'"
+end
+
+execute "Install the Percona repository" do
+  command "yum install -y http://www.percona.com/downloads/percona-release/redhat/0.1-6/percona-release-0.1-6.noarch.rpm"
+  not_if "yum repolist enabled | grep -E '^percona-release' > /dev/null"
 end
 
 package "bash-completion" do
   action :install
 end
 
-package "httpd" do
-  action :install
+%w(httpd php).each do |pkg|
+  package pkg do
+    action :install
+  end
 end
 
 service "httpd" do
